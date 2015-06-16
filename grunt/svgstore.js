5714f74e6ebf85c8b9b4d01740436bc2f26867db
Will Forsyth <will.forsyth@drummondcentral.co.uk>
2015-06-16 12:15:55 +0100

    initial commit

---
+++ grunt/svgstore.js
@@ -0,0 +1,16 @@
+module.exports = {
+    options: {
+        prefix: 'icon__',
+        svg: {
+            viewBox : '0 0 100 100',
+            xmlns: 'http://www.w3.org/2000/svg',
+            class: 'hide'
+        },
+        externalDefs: false
+    },
+    dev: {
+        files: {
+            'dev/_assets/imgs/sprite/icon-sprite.svg': ['dev/_assets/svg/*.svg'],
+        },
+    }
+};
